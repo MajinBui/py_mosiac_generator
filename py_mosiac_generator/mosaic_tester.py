3835f9259e8ec9a1a1329d109a575307a147f6ae
MajinBui <vanchau28@gmail.com>
2016-02-07 22:08:16 -0500

    Changed to test clusters

--- py_mosiac_generator/mosaic_tester.py
+++ py_mosiac_generator/mosaic_tester.py
@@ -1,7 +1,14 @@
 from PIL import Image, ImageStat
 import colorsys
 from mosaic import *
+from cluster_math import *
 
-icon_list = generate_icon_list("../images", "png")
+# icon_list = generate_icon_list("../images", "png")
+# generate_mosaic("./tests/sfarcana.jpg", icon_list).save("output.png")
 
-generate_mosaic("../sfarcana.jpg", icon_list).save("output.png")
+# test = Cluster([1, 1, 1], [1, 1, 1])
+# print(test)
+
+# clusterMap = ClusterMap()
+# image = Image.open("../images/Aegis_of_the_Immortal.png")
+normalize_picture("./tests/Eiffel-1.jpg", "./tests_normalize/output7.png", 5)
