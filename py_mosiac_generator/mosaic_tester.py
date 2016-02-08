e2d0e43bb997362f517024a6497635c851fd30b4
MajinBui <vanchau28@gmail.com>
2016-02-07 22:34:13 -0500

    updated to proper main

--- py_mosiac_generator/mosaic_tester.py
+++ py_mosiac_generator/mosaic_tester.py
@@ -4,11 +4,19 @@ from mosaic import *
 from cluster_math import *
 
 # icon_list = generate_icon_list("../images", "png")
-# generate_mosaic("./tests/sfarcana.jpg", icon_list).save("output.png")
 
-# test = Cluster([1, 1, 1], [1, 1, 1])
-# print(test)
+def main():
+    option = int(input("Press 1 to make a mosiac, press 2 to normalize a picture, 0 to exit: "))
+    input_image_path = str(input("Input image path: "))
+    output_image_path = str(input("Output image path: "))
 
-# clusterMap = ClusterMap()
-# image = Image.open("../images/Aegis_of_the_Immortal.png")
-normalize_picture("./tests/Eiffel-1.jpg", "./tests_normalize/output7.png", 5)
+    if option is 1:
+        generate_mosaic(input_image_path, icon_list).save(output_image_path)
+    if option is 2:
+        number_of_colors = int(input("Number of colors: "))
+        normalize_picture(input_image_path, output_image_path, number_of_colors)
+    if option is 99:
+        normalize_picture("./tests/sfarcana.jpg", "tests_normalize/output9.png", 5)
+
+if __name__ == "__main__":
+    main()
