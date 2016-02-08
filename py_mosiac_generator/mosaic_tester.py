2d31807635e2c0d055285ae4c53e2698ed00af52
MajinBui <vanchau28@gmail.com>
2016-02-08 15:51:13 -0500

    updated the tester to allow better options

--- py_mosiac_generator/mosaic_tester.py
+++ py_mosiac_generator/mosaic_tester.py
@@ -3,20 +3,22 @@ import colorsys
 from mosaic import *
 from cluster_math import *
 
-# icon_list = generate_icon_list("../images", "png")
 
 def main():
     option = int(input("Press 1 to make a mosiac, press 2 to normalize a picture, 0 to exit: "))
-    input_image_path = str(input("Input image path: "))
-    output_image_path = str(input("Output image path: "))
-
-    if option is 1:
-        generate_mosaic(input_image_path, icon_list).save(output_image_path)
-    if option is 2:
-        number_of_colors = int(input("Number of colors: "))
-        normalize_picture(input_image_path, output_image_path, number_of_colors)
     if option is 99:
-        normalize_picture("./tests/sfarcana.jpg", "tests_normalize/output9.png", 5)
+        normalize_picture("./tests/Eiffel-1.jpg", "tests_normalize/output14.png", 9)
+    elif option is 0:
+        exit()
+    else:
+        input_image_path = str(input("Input image path: "))
+        output_image_path = str(input("Output image path: "))
+        if option is 1:
+            icon_list = generate_icon_list("../images", "png")
+            generate_mosaic(input_image_path, icon_list).save(output_image_path)
+        if option is 2:
+            number_of_colors = int(input("Number of colors: "))
+            normalize_picture(input_image_path, output_image_path, number_of_colors)
 
 if __name__ == "__main__":
     main()
